770bda45fa048ad123112d69f179921b2294ac24
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2021-04-04 12:35:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div id="logo">
+        <span>
+          getGreeks
+        </span>
+      </div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  color: #2c3e50;
+  width: 100%;
+  height: 60px;
+  margin-top: 30px;
+  text-align: center;
+}
+
+#logo {
+  position: absolute;
+  text-align: left;
+}
+
+#nav a {
+  color: inherit;
+  text-decoration: none;
+  font-weight: bold;
+  border-bottom: 3px solid transparent;
+  transition: 0.5s;
+  position: relative;
+}
+
+#nav a:hover {
+  border-bottom-color: #42b983;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Options.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="options">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h3> Values of an option</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h5>Enter the value</h5>
+          <label> ahaha
+            <input name="test" type="number"/>
+          </label>
+        </div>
+        <div class="col">Test2</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Options',
+};
+</script>
+
+<style scoped>
+
+</style>
