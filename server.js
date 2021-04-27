11f85a3cf9cda6a1db33cfe6b83c31a4eba53707
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2021-04-27 14:44:09 +0200

    fix port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 const port = process.env.PORT || 8080;
-app.listen(8080);
+app.listen(port);
 
 console.log(`Listening on port: ${port}`);
